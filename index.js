d3fd50b8ed24053be988e002af5450b38faca710
Kyle Schrade <kyleschrade2@gmail.com>
2019-06-17 21:24:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const localPort = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! This is a full test of my pipeline!'))
 
 app.listen(process.env.PORT || localPort, () => console.log(`Example app listening on port ${localPort}!`))
